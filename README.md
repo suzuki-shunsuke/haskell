29c3f3f3d1b2248c6ab5408c52ddca772ef81282
Suzuki Shunsuke <suzuki.shunsuke.1989@gmail.com>
2016-05-01 14:55:32 +0900

    stack, wai, warpでhello world
    https://www.stackage.org/package/wai
    http://docs.haskellstack.org/en/stable/README/

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # haskell
+
+## Release
+
+### 0.1.0
+
+stack, wai, warpでhello worild
---
+++ wai-app/app/Main.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp (run)
+
+app :: Application
+app _ respond = do
+    putStrLn "I've done some IO here"
+    respond $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        "Hello, Web!"
+
+main :: IO ()
+main = do
+    putStrLn $ "http://localhost:8080/"
+    run 8080 app
